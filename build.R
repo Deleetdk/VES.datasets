f260abb660bd35693bb2aa71bcfabc1f492a2698
Emil O. W. Kirkegaard <emil@emilkirkegaard.dk>
2016-11-15 20:04:09 +0100

    Create package with the first dataset. First version.

---
+++ build.R
@@ -0,0 +1,14 @@
+library(pacman)
+p_load(devtools, roxygen2, readr)
+
+
+# update datasets ---------------------------------------------------------
+VES_general = read_rds("../data/VES_general.rds")
+use_data(VES_general, overwrite = T)
+
+# update package ----------------------------------------------------------
+document()
+
+devtools::install()
+
+library(VES.datasets)
